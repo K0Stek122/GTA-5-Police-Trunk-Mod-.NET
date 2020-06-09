3d3300cc5740de4ef3ee5d2e95ced1e418f84ef2
K0Stek122 <48300772+K0Stek122@users.noreply.github.com>
2020-06-09 18:37:14 +0100

    Add files via upload

---
+++ PoliceTrunk/PoliceTrunk/Spike.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using GTA;
+using GTA.Math;
+using GTA.Native;
+using NativeUI;
+
+namespace PoliceTrunk
+{
+    public class Spike
+    {
+        public Prop spike;
+
+        public Spike(Vector3 pos)
+        {
+            //spike = World.CreateProp("p_ld_stinger_s", pos, new Vector3(0, 0, 0), false, true);
+            spike = World.CreateProp("p_ld_stinger_s", pos, Game.Player.Character.Rotation, false, true);
+            spike.FreezePosition = true;
+        }
+
+        public List<Vehicle> GetVehiclesOnTop()
+        {
+            Vehicle[] vehicles = World.GetAllVehicles();
+            List<Vehicle> closeVehicles = new List<Vehicle>();
+
+            float closestDistance = 1.5f;
+            foreach (Vehicle vehicle in vehicles)
+            {
+                if (vehicle.Position.DistanceTo(this.spike.Position) <= closestDistance) //Gets Distance in between cone position and vehicle position
+                    closeVehicles.Add(vehicle); //If any vehicle is closer tha 2.5f, then it return this vehicle
+            }
+            return closeVehicles;
+        }
+    }
+}
---
+++ PoliceTrunk/PoliceTrunk/TrunkMenu.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using GTA;
+using GTA.Math;
+using GTA.Native;
+using NativeUI;
+
+namespace PoliceTrunk
+{
+    public class TrunkMenu
+    {
+        public UIMenu Menu;
+
+        public UIMenuCheckboxItem ConesItem;
+        public UIMenuCheckboxItem SpikeStripItem;
+
+        public UIMenuItem ClearSceneItem;
+
+        public UIMenuItem CloseTrunkItem;
+
+        public TrunkMenu()
+        {
+            this.Menu = new UIMenu("Police Trunk", "Choose an option");
+
+            this.Menu.AddItem(ConesItem = new UIMenuCheckboxItem("Cones", false));
+            this.Menu.AddItem(SpikeStripItem = new UIMenuCheckboxItem("Spike Strip", false));
+
+            this.Menu.AddItem(ClearSceneItem = new UIMenuItem("Clear Scene"));
+
+            this.Menu.AddItem(CloseTrunkItem = new UIMenuItem("Close Trunk"));
+
+            this.Menu.RefreshIndex();
+            this.Menu.MouseControlsEnabled = false;
+        }
+    }
+}
